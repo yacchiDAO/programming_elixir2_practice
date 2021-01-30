2ddafb216244aeda692fc3d231a184363eabc727
yacchiDAO <okamura@net.lab.uec.ac.jp>
2021-01-30 14:46:40 +0900

    add doc

--- practice11/strings_and_binaries6.exs
+++ practice11/strings_and_binaries6.exs
@@ -1,4 +1,16 @@
 defmodule StringsAndBinaries6 do
+  @moduledoc """
+    文字列を文章(. )で区切って戦闘を大文字に、それ以外を小文字に変換
+    """
+
+    @doc """
+    文章をわたすと変換されます
+
+    ## Examples
+
+      iex> StringsAndBinaries6.capitalize_sentences "Oh. A dog. Woof. "
+      "Oh. A dog. Woof. "
+  """
   def capitalize_sentences(str), do: String.split(str, ". ") |> _capitalize
 
   def _capitalize([]), do: ""
--- practice11/strings_and_binaries7.exs
+++ practice11/strings_and_binaries7.exs
@@ -1,4 +1,25 @@
 defmodule StringsAndBinaries7 do
+  @moduledoc """
+    特定のcsvをパースしてキーワードリストに変換
+  """
+
+  @doc """
+  ファイルパスを渡す
+
+  ## Examples
+
+    iex> StringsAndBinaries7.csv_parse("./practice11/strings_and_binaries7.csv")
+    [
+      [id: 123, ship_to: :NC, net_amount: 100.0],
+      [id: 124, ship_to: :OK, net_amount: 35.5],
+      [id: 125, ship_to: :TX, net_amount: 24.0],
+      [id: 126, ship_to: :TX, net_amount: 44.0],
+      [id: 127, ship_to: :NC, net_amount: 25.0],
+      [id: 128, ship_to: :MA, net_amount: 10.0],
+      [id: 129, ship_to: :CA, net_amount: 102.0],
+      [id: 130, ship_to: :NC, net_amount: 50.0]
+    ]
+  """
   def csv_parse(file_path) do
     { :ok, str } = File.read(file_path)
     [ header_str | data_str_list ] = str |> String.trim |> String.split("\n")

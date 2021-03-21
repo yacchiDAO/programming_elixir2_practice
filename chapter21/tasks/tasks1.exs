d284d7ed5cf9e55244d296c4ef33f207133e3926
yacchiDAO <okamura@net.lab.uec.ac.jp>
2021-03-21 22:00:00 +0900

    chapter 21

---
+++ chapter21/tasks/tasks1.exs
@@ -0,0 +1,18 @@
+defmodule Fib do
+  def of(0), do: 0
+  def of(1), do: 1
+  def of(n), do: Fib.of(n-1) + Fib.of(n-2)
+end
+
+IO.puts "Start"
+worker = Task.async(fn -> Fib.of(20) end)
+IO.puts "Do something"
+
+IO.puts "Wait for the task"
+result = Task.await(worker)
+IO.puts "The result is #{result}"
+
+# 2
+worker = Task.async(Fib, :of, [20])
+result = Task.await(worker)
+IO.puts "The result if #{result}"
